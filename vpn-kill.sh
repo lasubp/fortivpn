d77c3a2b43ec97a63ea717021377aecb3dc25dea
lasubp <147257735+lasubp@users.noreply.github.com>
2026-05-26 12:11:31 +0300

    output on vpn-kill

--- vpn-kill.sh
+++ vpn-kill.sh
@@ -3,3 +3,6 @@
 # Kill runninf opebfortivpn process running on backround
 sudo kill $(ps -e | grep openfortivpn | awk '{print $1}'); sleep 2;
 
+# Display log output to confirm that connection is down
+sudo cat /tmp/vpn-output.log | tail -n 15
+
